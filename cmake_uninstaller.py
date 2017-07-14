77a27cf8b35bd891e33de1df1c893ef2f64f8ad8
bcedu <eduard.bc.95@gmail.com>
2017-07-14 23:41:39 +0200

    Move cmake uninstaller and cmake help from build folder to main folder

---
+++ cmake_uninstaller.py
@@ -0,0 +1,13 @@
+
+import subprocess
+print("Warning! This script will delete all the files listed in install_manifest.txt.")
+print("It must be run as sudo.")
+if input("Do you wish to continue? (y/N)") == "y":
+    with open("build/install_manifest.txt", "r") as installed_files:
+        for installed_file in installed_files.readlines():
+            installed_file = installed_file.strip()
+            print("Deleting: {0}".format(installed_file))
+            try:
+                subprocess.call(["rm", installed_file])
+            except Exception as e:
+                print(e)
