aa5ba44e50ef8570ba206bb90d16af3570ce805e
bcedu <eduard.bc.95@gmail.com>
2017-07-17 17:49:19 +0200

    Improve cmake_uninstaller

--- cmake_uninstaller.py
+++ cmake_uninstaller.py
@@ -1,8 +1,9 @@
 
 import subprocess
+import os
 print("Warning! This script will delete all the files listed in install_manifest.txt.")
 print("It must be run as sudo.")
-if input("Do you wish to continue? (y/N)") == "y":
+if input("Do you wish to continue? (y/N)\n") == "y":
     with open("build/install_manifest.txt", "r") as installed_files:
         for installed_file in installed_files.readlines():
             installed_file = installed_file.strip()
@@ -11,3 +12,10 @@ if input("Do you wish to continue? (y/N)") == "y":
                 subprocess.call(["rm", installed_file])
             except Exception as e:
                 print(e)
+    if input("Uninstalled.\nDo you wish to clean 'build' and 'po' folders? (y/N)\n") == "y":
+        if os.path.exists("build"):
+            os.system("rm -r build/*")
+        else:
+            os.system("mkdir build")
+        if os.path.exists("po"):
+            os.system("rm -r po/*.po")
